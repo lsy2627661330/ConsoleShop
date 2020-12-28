2027ab1d5b7b0deee4f7b7a0867b1aca5a7bf9d3
lsy2627661330 <2627661330@qq.com>
2020-12-29 02:55:26 +0800

    2

--- src/ReadUserExcel.java
+++ src/ReadUserExcel.java
@@ -7,12 +7,14 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.text.DecimalFormat;
 
-public class ReadExcel {
-    public User[] readExcel(File file) {
-        User users[] = null;
+public class ReadUserExcel {
+    public User[] readExcel(InputStream in) {
+        User[] users = null;
         try {
-            XSSFWorkbook xw = new XSSFWorkbook(new FileInputStream(file));
+            XSSFWorkbook xw = new XSSFWorkbook(in);
             XSSFSheet xs = xw.getSheetAt(0);
             users = new User[xs.getLastRowNum()];
             for (int j = 1; j <= xs.getLastRowNum(); j++) {
@@ -43,7 +45,7 @@ public class ReadExcel {
 
     private String getValue(XSSFCell cell) {
         String value;
-        CellType type = cell.getCellTypeEnum();
+        CellType type = cell.getCellType();
 
         switch (type) {
             case STRING:
@@ -56,7 +58,9 @@ public class ReadExcel {
                 value = cell.getBooleanCellValue() + "";
                 break;
             case NUMERIC:
-                value = cell.getNumericCellValue() + "";
+                DecimalFormat df= new DecimalFormat("#");
+                value =df.format(cell.getNumericCellValue());
+                System.out.println(("处理后的：" + value));
                 break;
             case FORMULA:
                 value = cell.getCellFormula();
